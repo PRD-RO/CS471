a431f99039ad412ff0e0814d8b056764af2a312c
PRD-RO <ragahdalmotiri@gmail.com>
2026-09-17 20:49:38 +0300

    First book

--- bookwebsite/apps/bookmodule/urls.py
+++ bookwebsite/apps/bookmodule/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.index),
-path('index2/<val1>/', views.index2)
+    path('', views.index),
+    path('index2/<val1>/', views.index2),
+    path('<int:bookId>/', views.viewbook)
 ]
--- bookwebsite/apps/bookmodule/views.py
+++ bookwebsite/apps/bookmodule/views.py
@@ -14,3 +14,15 @@ def index2(request, val1=0):
         return HttpResponse(f"value1 = {val1}")
   except ValueError:
         return HttpResponse("error,expected val1 to be integer", status=400)
+
+
+def viewbook(request, bookId):
+    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
+    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+
+    targetBook = None
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
+
+    context = {'book': targetBook}
+    return render(request, 'bookmodule/show.html', context)
